643c81e3e0d1aee3af9042f7a07512b03c8a8a57
Diego-Bonora <bonoradi@gmail.com>
2024-04-11 17:35:43 -0300

    new structure for the project

---
+++ src/App/AppUI.js
@@ -0,0 +1,44 @@
+import { CreateTodoButton } from '../CreateTodoButton/CreateTodoButton';
+import { TodoCounter } from '../TodoCounter/TodoCounter';
+import { TodoItem } from '../TodoItem/TodoItem';
+import { TodoList } from '../TodoList/TodoList';
+import { TodoSearch } from '../TodoSearch/TodoSearch';
+
+function AppUI({
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+}) {
+    return (
+        <>
+
+            <TodoCounter total={totalTodos} completed={completedTodos} />
+            <TodoSearch
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+            />
+
+            <TodoList>
+                {searchedTodos.map(todo => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
+
+            <CreateTodoButton />
+
+
+        </>
+    );
+}
+
+export { AppUI }
